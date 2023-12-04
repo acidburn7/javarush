c560f7a9e0e23a12b93111c3136026771c952627
acidburn7 <varaksinaalla@mail.ru>
2023-12-04 22:04:49 +0300

    solving problems

---
+++ src/com/javarush/task/pro/task03/task0314/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task03.task0314;
+
+import java.util.Scanner;
+
+/* 
+Сломанная клавиатура
+*/
+
+public class Solution {
+    public static String secret = "AmIGo";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+
+        if (str.equalsIgnoreCase(secret)) {
+            System.out.println("доступ разрешен");
+        } else {
+            System.out.println("доступ запрещен");
+        }
+    }
+}
