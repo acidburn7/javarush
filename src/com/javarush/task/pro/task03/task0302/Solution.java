756e01e02eab22bb18687610c0affbfc39491d2d
acidburn7 <varaksinaalla@mail.ru>
2023-12-04 20:15:50 +0300

    solving problems

--- src/com/javarush/task/pro/task03/task0302/Solution.java
+++ src/com/javarush/task/pro/task03/task0302/Solution.java
@@ -10,5 +10,12 @@ public class Solution {
     public static void main(String[] args) {
         String militaryCommissar = ", явитесь в военкомат";
         //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int age = scanner.nextInt();
+
+        if (age >= 18 && age <= 28) {
+            System.out.println(name + militaryCommissar);
+        }
     }
 }
