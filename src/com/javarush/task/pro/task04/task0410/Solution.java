7204cf835ba406f361f021e6919f05d273265265
acidburn7 <varaksinaalla@mail.ru>
2023-12-08 19:06:53 +0300

    solving problems

--- src/com/javarush/task/pro/task04/task0410/Solution.java
+++ src/com/javarush/task/pro/task04/task0410/Solution.java
@@ -8,7 +8,21 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
 
+        int min = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
 
+        while (console.hasNextInt()) {
+            int tmp = console.nextInt();
+            if (tmp < min) {
+                secondMin = min;
+                min = tmp;
+            } else if (tmp > min && tmp < secondMin) {
+                secondMin = tmp;
+            }
+        }
+
+        System.out.println(secondMin);
     }
 }
