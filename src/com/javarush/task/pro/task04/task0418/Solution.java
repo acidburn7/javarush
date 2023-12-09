ec4ab47e9d96c71d0bffc00ccdaf2756fd590eb2
acidburn7 <varaksinaalla@mail.ru>
2023-12-09 23:11:05 +0300

    solving problems

--- src/com/javarush/task/pro/task04/task0418/Solution.java
+++ src/com/javarush/task/pro/task04/task0418/Solution.java
@@ -9,7 +9,11 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         double glass = 0.5;
-        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
 
+        boolean t = scanner.nextBoolean();
+
+        double result = t ? Math.ceil(glass) : Math.floor(glass);
+        System.out.println((int)result);
     }
 }
