a4fcb26f425ce81f58716950ae0bdc8ae97a6600
acidburn7 <varaksinaalla@mail.ru>
2023-12-10 22:14:33 +0300

    solving problems

--- src/com/javarush/task/pro/task05/task0508/Solution.java
+++ src/com/javarush/task/pro/task05/task0508/Solution.java
@@ -10,7 +10,24 @@ public class Solution {
     public static String[] strings;
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        strings = new String[6];
+        for (int i = 0; i < 6; i++) {
+            strings[i] = scanner.nextLine();
+        }
 
+        for (int i = 0; i < strings.length; i++) {
+            String current = strings[i];
+            for (int j = i + 1; j < strings.length; j++) {
+                if (current == null) {
+                    break;
+                }
+                if (current.equals(strings[j])) {
+                    strings[i] = null;
+                    strings[j] = null;
+                }
+            }
+        }
 
         for (int i = 0; i < strings.length; i++) {
             System.out.print(strings[i] + ", ");
