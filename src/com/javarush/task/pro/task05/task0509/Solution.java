b3bb830822998f99b0d435521b047cf5261ad8e6
acidburn7 <varaksinaalla@mail.ru>
2023-12-11 20:04:23 +0300

    solving problems

--- src/com/javarush/task/pro/task05/task0509/Solution.java
+++ src/com/javarush/task/pro/task05/task0509/Solution.java
@@ -9,6 +9,18 @@ public class Solution {
     public static int[][] MULTIPLICATION_TABLE;
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        MULTIPLICATION_TABLE = new int[10][10];
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                MULTIPLICATION_TABLE[i-1][j-1] = j * i;
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(MULTIPLICATION_TABLE[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
