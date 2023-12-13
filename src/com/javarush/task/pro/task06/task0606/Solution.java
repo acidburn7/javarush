fbd428169409f9768c59d0da792ee36c4e1e82b2
acidburn7 <varaksinaalla@mail.ru>
2023-12-13 15:47:38 +0300

    solving problems

--- src/com/javarush/task/pro/task06/task0606/Solution.java
+++ src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -14,5 +14,40 @@ public class Solution {
 
     }
 
-    //напишите тут ваш код
+    public static void universalMethod(String name){
+    }
+
+    public static void universalMethod(char[] nam2){
+
+    }
+
+    public static int universalMethod(String name, int age){
+        int b = 10;
+        return b;
+    }
+
+    public static float universalMethod(String name, String surname, int age){
+        float b = 10;
+        return b;
+    }
+
+    public static void universalMethod(String name, String surname, String lastname, int age){
+
+    }
+
+    public static void universalMethod(char[] name, String surname, String lastname, int age){
+
+    }
+
+    public static void universalMethod(String name, char[] surname, String lastname, int age){
+
+    }
+
+    public static void universalMethod(String name, String surname, char[] lastname, int age){
+
+    }
+
+    public static void universalMethod(String name, String surname, String lastname, int[] age){
+
+    }
 }
